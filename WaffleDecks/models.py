9ae83cd76e5070afafc4701f27862f8648b35ac8
Pablo A <paceiton1@gmail.com>
2023-06-01 11:27:28 -0400

    hechos algunos modelos

--- WaffleDecks/models.py
+++ WaffleDecks/models.py
@@ -1,7 +1,7 @@
 from django.db import models
-
+# Modelo usuario
 class Usuario(models.Model):
-  idUsuario = models.AutoField(db_column="idUsiario", primary_key=True)
+  idUsuario = models.AutoField(db_column="idUsuario", primary_key=True)
   runUsuario = models.CharField(db_column='runUsuario', max_length=10)
   primerNombre = models.CharField(max_length=20, blank=False, null=False)
   segundoNombre = models.CharField(max_length=20, blank=True, null=True)
@@ -10,4 +10,12 @@ class Usuario(models.Model):
   def __str__(self):
     return str(self.nombre + ' ' + self.primerNombre + ' ' + self.apPaterno + ' - '
                + self.run)
+  
+  #Modelo Deck 
+class Deck(models.Model):
+  idDeck = models.AutoField(db_column='idDeck', primary_key=True)
+  nombreDeck = models.CharField(max_length=30, blank=False, null=False)
+  precioDeck = models.IntegerField(blank=False, null=True)
+  descripcion = models.CharField(max_length=500, blank=False, null=False)
+  imagen_url = models.CharField(blank=False, null=False)
 # Create your models here.
